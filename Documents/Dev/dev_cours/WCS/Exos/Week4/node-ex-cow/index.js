9162fcd82ca71bc1b8b67d72c37bb3bd86e2a57a
Océane Lahoual <138216585+Midunighto@users.noreply.github.com>
2023-10-02 11:55:17 +0200

    forgot somethg

--- Documents/Dev/dev_cours/WCS/Exos/Week4/node-ex-cow/index.js
+++ Documents/Dev/dev_cours/WCS/Exos/Week4/node-ex-cow/index.js
@@ -2,10 +2,8 @@ const infos = require('./information.js');
 var cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : infos.name,
+    text : `Hello ${infos.name} from ${infos.campus}`,
     e : "oO",
     T : "U "
 }));
 
-
-console.log(infos)
